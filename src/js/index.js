ba13b171a79c343218634460b99e11fc07bd38c3
BacaCesar <cesarbaca.fotografia@gmail.com>
2024-06-25 11:24:09 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,76 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
 
-const passwordElement=document.getElementById('password');
-const lengthTextElement=document.getElementById('length-text');
-const rangeElement=document.getElementById('range');
-const buttonGenerateElement=document.getElementById('generate-password');
+const passwordElement = document.getElementById('password');
+const lengthTextElement = document.getElementById('length-text');
+const rangeElement = document.getElementById('range');
+const buttonGenerateElement = document.getElementById('generate-password');
 
-const allowedCharacters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/'
+const uppercaseInputElement = document.getElementById('uppercase');
+const lowercaseInputElement = document.getElementById('lowercase');
+const numbersInputElement = document.getElementById('numbers');
+const symbolsInputElement = document.getElementById('symbols');
 
-let passwordLength=rangeElement.value;
-let finalPassword='';
+const passwordOptions = {
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  numbers: '0123456789',
+  symbols: '!@#$%^&*()_+-={}[]:;<>,.?/'
+};
 
-const changeLengthText=event=>{
-    passwordLength=event.target.value
-    lengthTextElement.textContent=passwordLength;
-}
+let passwordLength = rangeElement.value;
+let allowedCharacters = '';
+let finalPassword = '';
 
-const generatePassword=()=>{
-    finalPassword='';
-    for(let i=0;i<passwordLength;i++){
-        const randomPosition=Math.floor(Math.random()*allowedCharacters.length);
-        const randomCharacter=allowedCharacters.charAt(randomPosition);
-        finalPassword += randomCharacter;
-    }
-    passwordElement.value = finalPassword
-}
-rangeElement.addEventListener('input',changeLengthText);
-buttonGenerateElement.addEventListener('click',generatePassword);
+const setDisabledButton = () => {
+  buttonGenerateElement.disabled = !allowedCharacters.length;
+};
+
+const fillAllowedCharacters = () => {
+  allowedCharacters = '';
+  const checkboxes = document.querySelectorAll('input:checked');
+
+  if (checkboxes.length === 0) {
+    return;
+  }
+
+  checkboxes.forEach(input => (allowedCharacters += passwordOptions[input.id]));
+
+  console.log(allowedCharacters);
+
+  setDisabledButton();
+};
+
+const changeLengthText = event => {
+  passwordLength = event.target.value;
+  lengthTextElement.textContent = passwordLength;
+};
+
+const generateRandomPosition = () => {
+  const randomPosition = Math.floor(Math.random() * allowedCharacters.length);
+  return randomPosition;
+};
+
+const getRandomCharacter = () => {
+  const randomPosition = generateRandomPosition();
+  const randomCharacter = allowedCharacters.charAt(randomPosition);
+  return randomCharacter;
+};
+
+const generatePassword = () => {
+  finalPassword = '';
+  for (let i = 0; i < passwordLength; i++) {
+    const randomCharacter = getRandomCharacter();
+    finalPassword += randomCharacter;
+  }
+
+  passwordElement.value = finalPassword;
+};
+
+rangeElement.addEventListener('input', changeLengthText);
+buttonGenerateElement.addEventListener('click', generatePassword);
+
+uppercaseInputElement.addEventListener('change', fillAllowedCharacters);
+lowercaseInputElement.addEventListener('change', fillAllowedCharacters);
+numbersInputElement.addEventListener('change', fillAllowedCharacters);
+symbolsInputElement.addEventListener('change', fillAllowedCharacters);
